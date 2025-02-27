6c4fc5f2209bfb57272b1120bcf02ebda5197bae
syed zumeer imam <164396686+zumeer006j@users.noreply.github.com>
2025-02-27 18:43:13 +0500

    Update growth.py

--- growth.py
+++ growth.py
@@ -61,7 +61,7 @@ if uploaded_files:
                             conversion_type = st.radio(f"convert{file.name} to:",["CSV","Excel"],key=file.name)
                             if st.button(f"Convert{file.name}"):
                               buffer = BytesIO()
-                                if conversion_type == "CSV":
+                                  if conversion_type == "CSV":
                                     df.to.csv(buffer, index=False)
                                     file_name = file.name.replace(file_ext,".csv")
                                     mime_type ="text/csv"
